8ccc31d3911f20fd1e80b663c87859ff794b356b
SIGEHgo <sigeh@hidalgo.gob.mx>
2025-09-25 11:59:58 -0600

    Se actualizo mapa web de correlaciones

--- Mapa_web_correlaciones.R
+++ Mapa_web_correlaciones.R
@@ -197,52 +197,100 @@ htmlwidgets::saveWidget(mapa, "Mapa_web_Correlaciones.html", selfcontained = T, 
 
 
 
+##################
+### Definitivo ###
+##################  
+
+proyeccion = raster::raster("Datos/carreteras_solo_proyeccion.tif")
+infraestructura = raster::raster("Datos/j_Percepción_infraestructura_vial.tif")
+viajes = raster::raster("Datos/nivel_de_uso_proxy_de_numero_de_viajes.tif")
+
+infraestructura[abs(infraestructura) <  1e-3] = NA
+viajes[viajes > 7000] = 7000
+
+infraestructura = raster::disaggregate(x = infraestructura, fact = 3)
+
+infraestructura = raster::projectRaster(infraestructura, crs = raster::crs(proyeccion)) 
+viajes = raster::projectRaster(viajes, crs = raster::crs(proyeccion))
 
 
+correlacion_pearson_7 = raster::raster("Datos/Correlaciones/correlacion_infraestructura_con_numero_de_viajes_7_pearson.tif")
 
+### Municipio 
+source("Funcion_Github.R")
+mun = sf::read_sf("../../Importantes_documentos_usar/Municipios/municipiosjair.shp")
+git = imagenes_git()
 
+mun = mun |> 
+  dplyr::select(CVEGEO, NOM_MUN) |> 
+  dplyr::left_join(y = git, by = c("NOM_MUN" = "nombre_principal")) |> 
+  relocate(links, .after = NOM_MUN)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-correlacion_pearson_3 = raster::raster("Datos/Correlaciones/correlacion_infraestructura_con_numero_de_viajes_3_pearson.tif")
+### Paletas de colores
 paleta_correlacion = leaflet::colorNumeric(
   palette = "Spectral", 
-  domain = , seq(-1,1,0.001),
+  domain = , seq(-1, 1, 0.001),
   na.color = "transparent",
   reverse = T
 )
 
+paleta_infraestructura = leaflet::colorNumeric(palette = "Spectral", 
+                                               domain = raster::values(infraestructura), 
+                                               na.color = "transparent", reverse = T)
+
+paleta_viajes = leaflet::colorNumeric(palette = "Spectral", 
+                                      domain = raster::values(viajes), 
+                                      na.color = "transparent", reverse = T)
 
 
+library(leaflet)
 library(leaflegend)
-
 
 mapa = leaflet() |> 
   addTiles() |> 
-  addRasterImage(correlacion_pearson_3, opacity = 0.7, group = "Pearson 3", colors = paleta_correlacion) |> 
+  addPolygons(data = mun, 
+              label = paste0(
+                "<div style='display:flex; align-items:center;'>",
+                "<img src='", mun$links, "' style='width:auto; height:30px; margin-right:5px;'>",
+                "<span>", "<b>", mun$NOM_MUN, "</b>", "</span>",
+                "</div>"
+              ) |> lapply(FUN =  function(x){htmltools::HTML(x)}),
+              color = "black",
+              fillColor = "black",
+              fillOpacity = 0.01,
+              weight = 1,
+              group = "municipios"
+  ) |> 
+  addRasterImage(infraestructura, opacity = 0.7, group = "Percepcion infraestructura vial", colors = paleta_infraestructura) |> 
+  addLegendNumeric(
+    pal = paleta_infraestructura, 
+    values = raster::values(infraestructura)[!is.na(raster::values(infraestructura))],  
+    title = "Percepcion infraestructura vial", position = "bottomright", group = "Percepcion infraestructura vial",
+    orientation = 'horizontal', shape = 'rect', decreasing = FALSE, height = 20, width = 200,
+    labels = c("Mala", "Buena"), tickLength = 0
+  ) |> 
+  addRasterImage(viajes, opacity = 0.7, group = "Nivel de uso por numero de viaje", colors = paleta_viajes) |> 
+  addLegendNumeric(
+    pal = paleta_viajes, 
+    values = raster::values(viajes)[!is.na(raster::values(viajes))],  
+    title = "Nivel de uso por numero de viaje", position = "bottomright", group = "Nivel de uso por numero de viaje",
+    orientation = 'horizontal', shape = 'rect', decreasing = FALSE, height = 20, width = 250,
+    labels = c("Baja", "Alta"), tickLength = 0
+  ) |> 
+  addRasterImage(correlacion_pearson_7, opacity = 0.7, group = "Correlacion Pearson", colors = paleta_correlacion) |> 
   addLegendNumeric(
     pal = paleta_correlacion, 
-    values = raster::values(correlacion_pearson_3)[!is.na(raster::values(correlacion_pearson_3))],  
-    title = "Pearson 3", position = "bottomright", group = "Pearson 3",
+    values = raster::values(correlacion_pearson_7)[!is.na(raster::values(correlacion_pearson_7))],  
+    title = "Correlacion Pearson", position = "bottomright", group = "Correlacion Pearson",
     orientation = 'horizontal', shape = 'rect', decreasing = FALSE, height = 20, width = 100,
     labels = c("-1", "1"), tickLength = 0
-  )
+  ) |> 
+  addLayersControl(
+    overlayGroups = c("Percepcion infraestructura vial", "Nivel de uso por numero de viaje", "Correlacion Pearson"),
+    options = layersControlOptions(collapsed = FALSE)
+  ) |> 
+  hideGroup(c("Nivel de uso por numero de viaje", "Correlacion Pearson"))  
 
 mapa
-  
+
+htmlwidgets::saveWidget(mapa, "Mapa_web_Correlaciones.html", selfcontained = T, title = "Correlaciones")
